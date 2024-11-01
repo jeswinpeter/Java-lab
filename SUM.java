3363e373f4a267141bfc15d206f21c972e2d312c
jeswin <jeswinpeter92@gmail.com>
2024-11-01 13:34:14 +0530

    Minor modifications

--- SUM.java
+++ SUM.java
@@ -1,7 +1,25 @@
+import java.lang.Math;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.lang.Math;
 public class SUM{
 	public static void main(String[] args){
-		int a = 5;
-		int b = 6;
-		System.out.println("a + b = " + (a + b)); 
+		int n = 6;
+		int i = 1;
+        Biginteger fact = 1;
+        while(i <= n) {
+            fact = fact * i;
+            i++;
+        }
+        System.out.println(fact); 
 	}
 }
